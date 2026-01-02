ec2f07f07969fefdb03aeb757beec1078503497b
Jeevan-code7 <jk29102023@gmail.com>
2026-01-02 17:01:56 +0530

    size of array

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int nums[] = {2,9};
+
+   int size = sizeof(nums)/sizeof(nums[0]);
+       int sum =  nums[0] + nums[size - 1];
+       cout << sum;
+
+    return 0;
+}
