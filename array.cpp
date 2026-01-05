7925e3e020856672c7662edb6eb4811441b6de6b
jeevan <jk29102023@gmail.com>
2026-01-05 20:23:38 +0530

    stl

--- array.cpp
+++ array.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 int main()
 {
-    int nums[] = {2,9};
-
-   int size = sizeof(nums)/sizeof(nums[0]);
-       int sum =  nums[0] + nums[size - 1];
-       cout << sum;
 
     return 0;
 }
---
+++ stl.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainpair()
+{
+    // pair<int, int> pr1 = {20, 2};
+    // cout << pr1.first << endl
+    //      << pr1.second << endl;
+
+    // pair<pair<int, char>, int> pr3 = {{2,'e'},7};
+    // cout << pr3.first.first <<pr3.first.second<<pr3.second;
+
+    // vector
+
+    /*vector<int> vec;
+   vec.push_back(20);
+    vec.push_back(89);
+    vec.emplace_back(90);
+
+    cout << vec.size()<<endl;
+    for(int i=0;i<=vec.size();i++){
+        cout << vec[i]<<endl;
+    }*/
+}
+
+int main()
+{
+    explainpair();
+    return 0;
+}
