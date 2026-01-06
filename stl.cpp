1910685b2856b51c649eba8ed80abf41dcdfbda8
jeevan <jk29102023@gmail.com>
2026-01-06 13:09:26 +0530

    heyyy stl

--- stl.cpp
+++ stl.cpp
@@ -21,6 +21,26 @@ void explainpair()
     for(int i=0;i<=vec.size();i++){
         cout << vec[i]<<endl;
     }*/
+
+    vector<int> vec;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    /*vector<int>::iterator beginitr = vec.begin();
+    vector<int>::iterator enditr = vec.end();
+    enditr = enditr - 1;
+    for (vector<int>::iterator i = beginitr; i <= enditr; i++)
+    {
+        cout << *i << " ";
+    }*/
+
+    auto reverseBegin = vec.rbegin();
+    auto reverseEnd = vec.rend();
+    for (auto i = reverseBegin; i < reverseEnd; i++)
+    {
+        cout << *i << " ";
+    }
 }
 
 int main()
